7cbadf3003ac5501019b279c1a919b2bdd9c56e9
zhilingl <zhilingl@ICT-HV8H6Q2>
2024-09-23 09:30:49 +1000

    Mylo

--- Week8/gragh.h
+++ Week8/gragh.h
@@ -1,1 +1,18 @@
-#pragma once
+typedef struct edge {
+	int to_vertex;
+	int weight;
+} Edge;
+
+typedef struct edgeNode {
+	Edge edge;
+	struct edgeNode* next;
+} *EdgeNodePtr;
+
+typedef struct edgeList {
+	EdgeNodePtr head;
+} EdgeList;
+
+typedef struct graph {
+	int V;
+	EdgeList* edges;
+} Graph;
